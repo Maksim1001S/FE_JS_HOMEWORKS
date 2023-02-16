bbc081ada89525b363d5286c19dda1e9ce9ee1be
Maksim1001S <93286670+Maksim1001S@users.noreply.github.com>
2023-02-16 22:11:28 +0100

    HW1

---
+++ homework1/scripts/srcipt.js
@@ -0,0 +1,19 @@
+// 1
+console.log("Task1");
+for (let i = 75; i <= 89; i++) {
+    console.log(i);
+    
+}
+// 2
+console.log("Task2");
+for (let i = 80; i >= 12; i -= 3) {
+    console.log(i);
+}
+// 3
+console.log("Task3");
+const ofDegree = (num, degree) => num ** degree;
+console.log(ofDegree(5,3));
+// 4
+console.log("Task4");
+const oneTenthOf = num => num/10;
+console.log(oneTenthOf(215));
